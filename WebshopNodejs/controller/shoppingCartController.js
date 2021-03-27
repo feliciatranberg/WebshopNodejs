4cd63300a7af2964b27026e34c0a4dfc9a1976e4
feliciatranberg <felicia@substansgbg.se>
2021-03-27 11:06:21 +0100

    some styling

--- WebshopNodejs/controller/shoppingCartController.js
+++ WebshopNodejs/controller/shoppingCartController.js
@@ -2,13 +2,6 @@ const Product = require("../model/product");
 const User = require("../model/user");
 require("dotenv").config();
 
-// const shoppingCartRender = async(req, res) => {
-   
-//     const user = await User.findOne({_id: req.user.user._id}).populate("shoppingCart");
-//     res.render("shoppingCart.ejs", {products: user.shoppingCart, _id: " "});
-//     console.log(user.shoppingCart);
-// }
-
 const addShoppingCart = async(req, res) => {
     try{
 
